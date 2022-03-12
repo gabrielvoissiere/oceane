5bc25728dbb5b0431c3c996ebb3685c253e8511e
gabrielvoissiere <gabvoissiere.pro@yahoo.com>
2022-03-12 14:47:33 +0100

    js fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // check if we are on index page
-if (window.location.pathname == '/') {
+if (document.title === 'wait...') {
     // redirect to /pages/accueil.html after 5 seconds
     setTimeout(() => {
         window.location.replace('../pages/accueil.html');
