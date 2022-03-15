65f9b8764b8a28e59bd1860672caa08e7ca6c4e6
gabrielvoissiere <gabvoissiere.pro@yahoo.com>
2022-03-15 14:49:03 +0100

    demo 2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 if (document.title === 'wait...') {
     // redirect to /pages/accueil.html after 5 seconds
     setTimeout(() => {
-        window.location.href = '../pages/accueil.html';
+        window.location.href = 'https://gabrielvoissiere.github.io/oceane/pages/accueil.html';
     }, 5000);
 }
+// replace 'https://gabrielvoissiere.github.io/oceane/pages/accueil.html' by '../pages/accueil.html' for dev
 
